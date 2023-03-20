2151c2e6e41e91bf1445a13cfa88abd4d45fa303
slacktracer <slacktracer@gmail.com>
2023-03-20 18:00:26 -0300

    I finally updated all the accounts to have 0 initial balance and have an initial income operation to set the starting balance.

--- src/data/initial-amounts-by-account.js
+++ src/data/initial-amounts-by-account.js
@@ -1,7 +1,7 @@
 export const initialAmountsByAccount = {
-  BB: -145,
-  Caixa: 267364,
-  Carteira: 2007,
-  Mother: 1101109,
-  "Poupança BB V1": 3444,
+  BB: 0,
+  Caixa: 0,
+  Carteira: 0,
+  Mother: 0,
+  "Poupança BB V1": 0,
 };
